d0f07d864bb1a977909a0a7de2f0fbeebf0d1b41
lihua <156557548@qq.com>
2023-12-24 00:55:07 +0800

    完善调试脚本

--- scripts/shell/gen_mingw.bat
+++ scripts/shell/gen_mingw.bat
@@ -1,2 +1,3 @@
 mkdir build
+set ARM_GCC_PATH=%cd%/tool/gcc/bin
 cmake -S . -B ./build -G "Ninja"
--- source/tests/test_sysctrl/test_sysctrl.c
+++ source/tests/test_sysctrl/test_sysctrl.c
@@ -7,8 +7,16 @@ int main(void)
     sysctrl_rch_trim_set(sysctrl_rch_freq_24M);
     sysctrl_clk_source_switch(sysctrl_clk_source_rch);
 
+    volatile uint32_t i = 0;
+
     while (1)
     {
+        i++;
+        if (i > 1000000)
+        {
+            sysctrl_rch_trim_set(sysctrl_rch_freq_4M);
+            i = 0;
+        }
     }
     return 0;
 }
